60c47b6ea3728f3ee6647fd04f5523c727ba999f
Joerg <joerg.doepfert@flixbus.com>
2017-07-10 23:38:13 +0200

    adapted notebook yaml and sh

--- start-notebook.sh
+++ start-notebook.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-exec su $BASICUSER -c "env PATH=$PATH jupyter notebook --ip='*' --no-browser $*"
+mkdir ~/.jupyter
+cd ~/.jupyter
+touch jupyter_notebook_config.py
+cat <<EOF >  jupyter_notebook_config.py
+c.NotebookApp.password = ''
+c.NotebookApp.token = ''
+c.NotebookApp.allow_origin = '*'
+EOF
+
+
+exec su $BASICUSER -c "env PATH=$PATH jupyter notebook --ip='*' --allow-root --no-browser $*"
